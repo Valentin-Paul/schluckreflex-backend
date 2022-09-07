bdbb3dd3cff3720e0672faa31129afd82c52cdfd
Valentin Paul <valentin@vamptech.de>
2022-09-07 12:11:06 +0200

    set usermodels

---
+++ models/User.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema (
+    {
+        name:{
+            type: String,
+            required: true
+        },
+        password:{
+            type: String,
+            required: true
+        }
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true,
+      }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
